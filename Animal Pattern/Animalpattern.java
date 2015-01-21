ad12c5424a5ad8960b88cb6d8a995b1093ae3bad
aswinaryal <aswinaryal@gmail.com>
2015-01-21 13:08:21 +0545

    first commit was attempted.

---
+++ Animal Pattern/Animalpattern.java
@@ -0,0 +1,14 @@
+
+public class Animalpattern {
+    public static void main(String[] args){
+
+        System.out.println("            ' _ ' ");
+        System.out.println("            (\u00A9\u00A9)");
+        System.out.println("  / ======== \\/ ");
+        System.out.println(" /  || %% ||");
+        System.out.println("*   ||----||");
+        System.out.println("    \u00A5\u00A5    \u00A5\u00A5");
+        System.out.println("    \"\"    \"\"");
+    }
+
+}
